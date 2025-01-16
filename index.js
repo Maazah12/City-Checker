8d9c42ee6084af4f26e64cb1ffe7b577e3fcf89c
Maaz Ahmed Khan <amaaz5315@gmail.com>
2025-01-16 21:50:02 +0500

    index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+const cities = [
+    "Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan",
+    "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
+    "Islamabad", "Sialkot", "Bahawalpur", "Sukkur",
+    "Larkana", "Mingora", "Sahiwal", "Dera Ghazi Khan",
+    "Mirpur Khas", "Chiniot", "Okara"
+  ];
+  
+  if (cities.includes("Islamabad")) {
+    alert("Islamabad is the cleanest city of Pakistan!");
+  } else {
+    alert("Islamabad is not found in the list of cities.");
+  }
+  
